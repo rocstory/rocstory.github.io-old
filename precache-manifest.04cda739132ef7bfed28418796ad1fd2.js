ae44684f675670a84b7c2c6c8783b8c3ec62bec9
Malik Roc <maliktroc@outlook.com>
2020-08-14 13:12:40 -0400

    Updates

---
+++ precache-manifest.04cda739132ef7bfed28418796ad1fd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2585b1407ec12e9d851080fe92e11cd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "530665b1186c12bfbf68",
+    "url": "/static/css/main.04f8b262.chunk.css"
+  },
+  {
+    "revision": "1819aae6edeab11f845f",
+    "url": "/static/js/2.aa28a03c.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.aa28a03c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530665b1186c12bfbf68",
+    "url": "/static/js/main.49cd72e4.chunk.js"
+  },
+  {
+    "revision": "3a1ab1879100d3e6df87",
+    "url": "/static/js/runtime-main.9d5af085.js"
+  },
+  {
+    "revision": "a61f806601a3912276796bff6c9311ee",
+    "url": "/static/media/cardboard.a61f8066.png"
+  },
+  {
+    "revision": "13fce2f5a73347975af1d81aab30026e",
+    "url": "/static/media/rsicon_full.13fce2f5.png"
+  }
+]);
