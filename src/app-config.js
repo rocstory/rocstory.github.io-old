30de1eb701d0343c0e00ffcdbac019dbd2d97565
Malik R <mrocstory@gmail.com>
2022-05-07 12:48:58 -0400

    responsiveness + tss entry

---
+++ src/app-config.js
@@ -0,0 +1,25 @@
+var configData = require('./app-config.json');
+
+export async function getConfig(key) {
+    try 
+    {
+        const value = configData[key];
+        return value;
+    }
+    catch(error)
+    {
+        return null;
+    }
+}
+export async function getConfigObj()
+{
+    try 
+    {
+        return configData;
+
+    }
+    catch (err)
+    {
+        return null; 
+    }
+}
