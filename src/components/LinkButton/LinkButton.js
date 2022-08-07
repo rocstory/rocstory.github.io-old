9257d348a3fff1df32d60f27058019dd4d4db8eb
Malik R <mrocstory@gmail.com>
2022-08-06 23:53:15 -0400

    colorful link buttons + centered

--- src/components/LinkButton/LinkButton.js
+++ src/components/LinkButton/LinkButton.js
@@ -1,36 +1,39 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './LinkButton.css';
 
 import { Button } from 'react-bootstrap';
 
 
-function LinkButton({ linkUrl, linkLabel }) {
+function LinkButton({ link }) {
+  const { label, url, icon, color } = link
 
-
+  const bgColor = { backgroundColor: color, border: color };
 
   return (
     <Button
       className="link-button"
+      style={bgColor}
     >
-      <span>{linkLabel}</span>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span
+          className={'link-icon float-left'}
+        >
+          <FontAwesomeIcon icon={[icon.type, icon.name]} />
+        </span>
+
+        <span
+          className={'link-label'}
+        >
+          {label}
+        </span>
+      </a>
     </Button>
   )
-
-
 }
 
 export default LinkButton;
-
-
-// return (
-//   <Button
-//     variant={null}
-//     className="link-button"
-//     size="lg"
-//     href={linkUrl}
-//     target="_blank"
-//     rel="noopener noreferrer"
-//   >
-//     <span>{linkLabel}</span>
-//   </Button>
-// )
--- src/pages/LinksPage/LinksPage.js
+++ src/pages/LinksPage/LinksPage.js
@@ -1,33 +1,28 @@
-import React, { useEffect, useContext, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import LinkButton from '../../components/LinkButton/LinkButton';
 import './LinksPage.css';
 
 const dataStore = require("../../dataStore/dataStore")
 
-// import {PortfolioContext} from "./contexts/PortfolioContext";
-
-function LinksPage()
-{
+function LinksPage() {
   const [links, setLinks] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    async function getLinks()
-    {
+    async function getLinks() {
       const linksData = dataStore.getLinksData();
       setLinks(linksData)
     }
     getLinks();
   }, [])
-  
+
   return (
     <div className="links-page">
       {
-        links.map(link => 
-          <LinkButton 
-            key={link.url} 
-            linkUrl={link.url}
-            linkLabel={link.label}
+        links.map(link =>
+          <LinkButton
+            key={link.url}
+            link={link}
           />)
       }
 
