12e87975a847f407e1996477fc7cc7ebf4d1a4d6
Malik Roc <maliktroc@outlook.com>
2023-03-25 13:49:28 -0400

    Hello project thumbnails

--- src/hooks/useEntryConfig.tsx
+++ src/hooks/useEntryConfig.tsx
@@ -11,7 +11,10 @@ export const useEntryConfig = ({entryId} : EntryConfigProp) => {
     const [thumbnail, setThumbnail] = useState(undefined);
     const {
         twinlandeThumbnail,
-        projectDefaultThumbnail
+        projectDefaultThumbnail,
+        chevEscapeThumbnail,
+        pawbytesCafeThumbnail,
+        colonialCarnivalThumbnail
     } = useImageRetriever()
 
     const getThumbnail = (entryId: string) => {
@@ -19,6 +22,12 @@ export const useEntryConfig = ({entryId} : EntryConfigProp) => {
         switch (entryId) {
             case EProjectId.TwinlandeSuperStore:
                 return twinlandeThumbnail;
+            case EProjectId.ChevEscape:
+                return chevEscapeThumbnail;
+            case EProjectId.PawBytes:
+                return pawbytesCafeThumbnail;
+            case EProjectId.ColonialCarnival:
+                return colonialCarnivalThumbnail
             default:
                 return projectDefaultThumbnail;
         }
@@ -26,7 +35,7 @@ export const useEntryConfig = ({entryId} : EntryConfigProp) => {
 
     useEffect(() => {
         const thmbnail = getThumbnail(entryId);
-        
+
         setThumbnail(thmbnail);
     }, [])
 
--- src/hooks/useImageRetriever.tsx
+++ src/hooks/useImageRetriever.tsx
@@ -1,15 +1,25 @@
 import {useContext} from 'react'
 import { EntryContext } from '../contexts/EntryContext'
 
-// images
+// banners
 import headerBanner from "../assets/website-bg-compressed.jpg"
+
+// thumbnails
 import projectDefaultThumbnail from "../assets/thumbnails/default-thumbnail_compressed.jpg"
 import twinlandeThumbnail from "../assets/thumbnails/twinlande-thumbnail_compressed.jpg"
+import chevEscapeThumbnail from  "../assets/thumbnails/chev-escape-thumbnail.jpg";
+import colonialCarnivalThumbnail from "../assets/thumbnails/colonial-carnival-thmbnl.jpg";
+import pawbytesCafeThumbnail from "../assets/thumbnails/pawbytes--cafe-thmbnl.jpg";
+
+
 
 export const useImageRetriever = () => {
   return {
     headerBanner,
     projectDefaultThumbnail,
-    twinlandeThumbnail
+    twinlandeThumbnail,
+    chevEscapeThumbnail,
+    colonialCarnivalThumbnail,
+    pawbytesCafeThumbnail
   }
 }
