5aa9aed95b2c4e8215fe292d3c71fa0dd058296d
Malik R <mrocstory@gmail.com>
2023-07-02 21:11:37 -0400

    phase 4 - rs icon cmp created

--- src/components/RSIcon/RSIcon.tsx
+++ src/components/RSIcon/RSIcon.tsx
@@ -2,12 +2,88 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, {  useContext, useState } from 'react';
 
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { ERSIcon } from '../../enums/ERSIcons';
+import { 
+    faGithub, faCuttlefish, faCss3Alt,
+    faHtml5, faJsSquare, faPython,
+    faRaspberryPi, faTwitter, faTwitch,
+    faInstagram, faFacebook, faYoutube,
+    faLinkedin, faGithubAlt, faUnity,
 
+} from '@fortawesome/free-brands-svg-icons';
+import { 
+    faPlay, faFire, faDatabase,
+    faRobot,
+    faEllipsisH,
+    faQuestionCircle,
+ 
+} from '@fortawesome/free-solid-svg-icons';
+import { 
+    faFileCode, faLightbulb
 
-function RSIcon() {
+} from '@fortawesome/free-regular-svg-icons';
+
+type RSIconProps = {
+    iconName: ERSIcon,
+}
+function RSIcon(props: RSIconProps) {
+    const {
+        iconName
+    } = props
+    const getFontAwesomeIcon = () => {
+        switch (iconName) {
+            case ERSIcon.Github:
+                return faGithub;
+            case ERSIcon.CuttleFish:
+                return faCuttlefish;
+            case ERSIcon.CSS:
+                return faCss3Alt; 
+            case ERSIcon.HTML:
+                return faHtml5;
+            case ERSIcon.JsSquare: 
+                return faJsSquare;
+            case ERSIcon.Python:
+                return faPython
+            case ERSIcon.RaspberryPi:
+                return faRaspberryPi;
+            case ERSIcon.Twitter:
+                return faTwitter
+            case ERSIcon.Twitch:
+                return faTwitch
+            case ERSIcon.Instagram:
+                return faInstagram;
+            case ERSIcon.Facebook:
+                return faFacebook;
+            case ERSIcon.Youtube:
+                return faYoutube;
+            case ERSIcon.LinkedIn:
+                return faLinkedin;
+            case ERSIcon.Unity:
+                return faUnity;
+            case ERSIcon.FileCode:
+                return faFileCode;
+            case ERSIcon.LightBulb:
+                return faLightbulb;
+            case ERSIcon.EllipsisH:
+                return faEllipsisH;
+            case ERSIcon.Play:
+                return faPlay;
+            case ERSIcon.Fire:
+                return faFire;
+            case ERSIcon.Database:
+                return faDatabase;
+            case ERSIcon.Robot:
+                return faRobot;
+            case ERSIcon.Other:
+                return faQuestionCircle
+            default:
+                return faLightbulb
+        }
+    }
+
+    const icon = getFontAwesomeIcon(); 
     return (
-        <FontAwesomeIcon icon={faGithub}/>
+        <FontAwesomeIcon icon={icon}/>
     )
 }
 
--- src/enums/ERSIcons.tsx
+++ src/enums/ERSIcons.tsx
@@ -1,14 +1,32 @@
 export enum ERSIcon {
+
+    // fab - brands
+    Github = "github",
+    CuttleFish = 'cuttle-fish',
+    CSS = 'css',
+    HTML = 'html',
+    JsSquare = 'javascript-square',
+    Python = 'python',
+    RaspberryPi = 'raspberry-pi',
+    Twitter = 'twitter',
+    Twitch = 'twitch',
+    Instagram = 'instagram',
+    Facebook = 'facebook',
+    Youtube = 'youtube',
+    LinkedIn = 'linkedin',
+    Unity = 'unity',
+
+    // fas - solid
+    FileCode = 'file-code',
+    LightBulb = 'light-bulb', 
+
     EllipsisH = "ellipsis-h",
     Play = "play",
+    Fire = "fire",
+    Database = "database",
+    Robot = "robot",
+    Other = "other"
 
-    // fab
-    Github = "github",
     
     
-}
-
-export enum ERSIconType {
-    Solid = "fas",
-    Brand = "fab"
 }
