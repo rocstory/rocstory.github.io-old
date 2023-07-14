8af91a2f8f26d1e8a98bb086bb2c6ffaff3d8a91
Malik Roc <maliktroc@outlook.com>
2023-07-14 13:08:56 -0400

    p4 - Colonial Carnival article update

--- src/components/Articles/ColonialCarnivalArticle/ColonialCarnivalArticle.tsx
+++ src/components/Articles/ColonialCarnivalArticle/ColonialCarnivalArticle.tsx
@@ -140,31 +140,27 @@ function AboutTabBody() {
             <div>
                 <h2>What is Colonial Carnival?</h2>
                 <p>
-                    Colonial Carnival is a web application built for the residents at Western Connecticut
-                    State University (WCSU).
+                    Colonial Carnival is a web application built for 
+                    the residents at Western Connecticut State University (WCSU).
+                </p>
+                <p>
+                    Colonial Carnival is a virtual carnival for students at WCSU to earn tickets 
+                    after each game to exchange for prizes such as gift cards, stationery, and other items.
                 </p>
             </div>
             <div>
                 <h2>What is the goal of this project?</h2>
                 <p>
-                    The goal of Colonial Carnival is to learn the fundamentals of website development.
-                    I also wanted to challenge myself by bringing a
-                    completely different type of Resident Assistant program to WCSU.
+                    The goal of Colonial Carnival is to learn the 
+                    fundamentals of website development. 
+                    I also wanted to challenge myself by bringing a completely different type of Resident Assistant program to WCSU.
                 </p>
             </div>
             <div>
                 <h2>Why did I create this project?</h2>
                 <p>
-                    I created Colonial Carnival to learn HTML, CSS, and JavaScript.
-                    I also wanted to bring a new program to the residents of WCSU.
-                </p>
-            </div>
-            <div>
-                <h2>What is this project about?</h2>
-                <p>
-                    Colonial Carnival is a virtual carnival for students
-                    at WCSU to earn tickets after each game
-                    to exchange for prizes such as gift cards, stationery, and other items.
+                I created Colonial Carnival to learn HTML, CSS, and JavaScript.
+                I also wanted to bring a new program to the residents of WCSU.
                 </p>
             </div>
         </div>
