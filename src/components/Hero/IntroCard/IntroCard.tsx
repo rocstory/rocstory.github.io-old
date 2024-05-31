44f472b7b16e188d32a6c789f11e487e2b100c6a
Malik R <mrocstory@gmail.com>
2024-05-31 02:48:14 -0400

    053124 -  Intro update

--- src/components/Hero/IntroCard/IntroCard.tsx
+++ src/components/Hero/IntroCard/IntroCard.tsx
@@ -9,7 +9,7 @@ function IntroCard() {
             <h2 className=" my-name">Malik Roc</h2>
             <div className="intro-paragraph">
                 <p>
-                    Hi! I'm Malik, a software engineer driven by creativity
+                    Hi! I'm a software engineer driven by creativity
                     and a passion for solving problems. Whether you're a developer, recruiter, or
                     simply curious, I invite you to look through the projects
                     I'm a part of.
