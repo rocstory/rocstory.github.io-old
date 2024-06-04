394a3573ce632f42ffee2bd315538ac2c48eb744
Malik R <mrocstory@gmail.com>
2024-06-04 01:28:38 -0400

    060424 - updated default thumbnail

--- src/helpers/imageHelper.tsx
+++ src/helpers/imageHelper.tsx
@@ -1,6 +1,6 @@
 import ProfilePicture from "../assets/imgs/profile-photo.png";
 import HeroImg from "../assets/imgs/rs-test-hero4.jpg";
-import DefaultThumbnail from "../assets/imgs/rsIcon.jpg";
+import DefaultThumbnail from "../assets/imgs/default-thumbnail_compressed.jpg";
 
 export {
     ProfilePicture,
